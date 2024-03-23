dc01d1943dc619b21e2d5dbf0bc1c86c5cf4a77b
rinomorin <rinomorin@gmail.com>
2024-03-23 01:01:33 -0400

    restart from kvm system

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get("/api/hosts", (req,  res) => {
-    res.json({domain_groups_data: [
+    res.json({domain_groups_data: [{
         "domain": "MorinSoft",
         "members": [
             {"host": [{"name": "morin-kvm01", "ip": "192.168.100.3", "user_id": "lv-user", "password": "password1", "site": "Site 1"}]},
             {"host": [{"name": "morin-kvm02", "ip": "192.168.100.4", "user_id": "lv-user", "password": "password1", "site": "Site 2"}]},
             {"host": [{"name": "morin-kvm03", "ip": "192.168.100.5", "user_id": "lv-user", "password": "password1", "site": "Site 3"}]}
-        ]
+        ]}
     ]})
 })
 app.listen(4001,() => {console.log("server listening on port 4001")})

41a71a72214cc13e3768a93766b2f9f3628b1402
rinomorin <rinomorin@gmail.com>
2024-03-23 02:30:35 -0400

    restart from kvm system

--- client/src/components/KvmDeviceList.js
+++ client/src/components/KvmDeviceList.js
@@ -1,38 +1,54 @@
 import React, { useState, useEffect } from 'react';
 
 function KvmDeviceList() {
-  const [domainGroupsData, setDomainGroupsData] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('/api/domain_groups_data')
+    fetch('/api/data')
       .then(response => response.json())
-      .then(data => setDomainGroupsData(data))
-      .catch(error => console.error('Error fetching domain groups data:', error));
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
   }, []);
 
   return (
     <div>
-      <h2>Hosts</h2>
-      <ul>
-        {domainGroupsData.map(group => (
-          <li key={group.domain}>
-            <h3>{group.domain}</h3>
-            <ul>
-              {group.members.map(member => (
-                <li key={member.host[0].name}>
-                  <strong>Name:</strong> {member.host[0].name}<br />
-                  <strong>IP:</strong> {member.host[0].ip}<br />
-                  <strong>User ID:</strong> {member.host[0].user_id}<br />
-                  <strong>Password:</strong> {member.host[0].password}<br />
-                  <strong>Site:</strong> {member.host[0].site}<br />
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+      <h1>Data from database1</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
+
+  // const [domainGroupsData, setDomainGroupsData] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('/api/domain_groups_data')
+  //     .then(response => response.json())
+  //     .then(data => setDomainGroupsData(data))
+  //     .catch(error => console.error('Error fetching domain groups data:', error));
+  // }, []);
+
+  // return (
+  //   <div>
+  //     <h2>Hosts</h2>
+  //     <ul>
+  //       {domainGroupsData.map(group => (
+  //         <li key={group.domain}>
+  //           <h3>{group.domain}</h3>
+  //           <ul>
+  //             {group.members.map(member => (
+  //               <li key={member.host[0].name}>
+  //                 <strong>Name:</strong> {member.host[0].name}<br />
+  //                 <strong>IP:</strong> {member.host[0].ip}<br />
+  //                 <strong>User ID:</strong> {member.host[0].user_id}<br />
+  //                 <strong>Password:</strong> {member.host[0].password}<br />
+  //                 <strong>Site:</strong> {member.host[0].site}<br />
+  //               </li>
+  //             ))}
+  //           </ul>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // );
 }
 
 export default KvmDeviceList;

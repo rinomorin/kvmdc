a0b8159d67be7b273a8f99090622d07d2f43c356
rinomorin <rinomorin@gmail.com>
2024-03-22 23:36:46 -0400

    restart from kvm system

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react'
 
 function App() {
-  const [BackendData, setBackendData] = useState[{}]
+
+  const [BackendData, setBackendData] = useState([{}])
 
   useEffect(() => {
     fetch("/api/hosts").then(

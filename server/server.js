e06cab717d20c11e849959969572834209b19c25
rinomorin <rinomorin@gmail.com>
2024-03-23 00:40:34 -0400

    restart from kvm system

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.get("/api/hosts", (req,  res) => {
 
-    res.json({nodes: ["morin-kvm01","morin-kvm02","morin-kvm03"]})
+    res.json({hosts: ["morin-kvm01","morin-kvm02","morin-kvm03"]})
 })
 app.listen(4001,() => {console.log("server listening on port 4001")})

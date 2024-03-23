9e8458b6fa73016e37e4159e9880bdb196d7971a
rinomorin <rinomorin@gmail.com>
2024-03-23 01:32:43 -0400

    restart from kvm system

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb');
 const app = express();
 
 // const MONGO_URI = "mongodb://localhost:27017/datacenter"

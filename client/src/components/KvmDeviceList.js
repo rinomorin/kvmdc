90b87ddafec04620eccb3635c6ef79720b3bc89c
Rino Morin <rmorin@ca.ibm.com>
2024-03-25 16:30:41 -0400

    working on tree

--- client/src/components/KvmDeviceList.js
+++ client/src/components/KvmDeviceList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 function KvmDeviceList() {
-  // const [collections, setCollections] = useState([]);
+  const [collections, setCollections] = useState([]);
 
   useEffect(() => {
     fetch('/api/collections')
